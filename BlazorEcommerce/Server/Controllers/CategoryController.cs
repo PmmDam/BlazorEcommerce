4b87f4fbba2fc31777843d272c623514e9c8e316
Pablo Muñoz <699328@alu.murciaeduca.es>
2023-05-27 16:10:36 +0200

    More propeties Added to the category model and migration with entity framework.  Also, added crud operations into category service and controller on the server side

--- BlazorEcommerce/Server/Controllers/CategoryController.cs
+++ BlazorEcommerce/Server/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers
@@ -19,6 +20,33 @@ namespace BlazorEcommerce.Server.Controllers
         {
             var result = await _categoryService.GetCategoriesAsync();
             return Ok(result);  
+        }
+        [HttpGet("admin"),Authorize(Roles ="Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> GetAdminCategories()
+        {
+            var result = await _categoryService.GetAdminCategoriesAsync();
+            return Ok(result);
+        }
+        [HttpDelete("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> DeleteCategory(int id)
+        {
+            var result = await _categoryService.DeleteCategoryAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> AddCategory(Category category)
+        {
+            var result = await _categoryService.AddCategoryAsync(category);
+            return Ok(result);
+        }
+
+
+        [HttpPut("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> UpdateCategory(Category category)
+        {
+            var result = await _categoryService.AddCategoryAsync(category);
+            return Ok(result);
         }
     }
 }
--- BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
+++ BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
@@ -9,13 +9,102 @@
             _context = context;
         }
 
-        public async Task<ServiceResponse<List<Category>>> GetCategoriesAsync()
+        /// <summary>
+        /// Añadimos una categoría a su repositorio correspondiente
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        public async Task<ServiceResponse<List<Category>>> AddCategoryAsync(Category category)
+        {
+            //Actualizamos las flags de esta categoría para no tener problemas en el front
+            category.Editing = false;
+            category.IsNew = false;
+
+            //Añadimos la categoría al repository
+            _context.Categories.Add(category);
+
+            //Commiteamos los cambios a la sb
+            await _context.SaveChangesAsync();
+
+            //Devolvemos un service response con todas las categorías
+            return await GetAdminCategoriesAsync();
+        }
+
+
+        /// <summary>
+        /// Borramos una categoría en función del ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<ServiceResponse<List<Category>>> DeleteCategoryAsync(int id)
+        {
+            var category = await GetCategoryById(id);
+
+            //Clausula guarda
+            if (category == null)
+            {
+                return CategoryNotFoundResponse();
+
+            }
+            category.Deleted = true;
+            await _context.SaveChangesAsync();
+
+            //Por ahorrar codigo, esto al final devuelve un service response con todas las categorías
+            return await GetAdminCategoriesAsync();
+        }
+
+        private static ServiceResponse<List<Category>> CategoryNotFoundResponse()
+        {
+            return new ServiceResponse<List<Category>>
+            {
+                Success = false,
+                Message = "Categoría no encontrada"
+            };
+        }
+
+        /// <summary>
+        /// Devuelve una categoría en función de su ID 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Category> GetCategoryById(int id)
+        {
+            return await _context.Categories.FirstOrDefaultAsync(category => category.Id == id);
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetAdminCategoriesAsync()
         {
             var categories = await _context.Categories.ToListAsync();
             return new ServiceResponse<List<Category>>()
             {
                 Data = categories
             };
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetCategoriesAsync()
+        {
+            var categories = await _context.Categories.Where(category => !category.Deleted && category.Visible).ToListAsync();
+            return new ServiceResponse<List<Category>>()
+            {
+                Data = categories
+            };
+        }
+
+        public async Task<ServiceResponse<List<Category>>> UpdateCategoryAsync(Category category)
+        {
+            var dbCategory = await GetCategoryById(category.Id);
+            if(category == null)
+            {
+                return CategoryNotFoundResponse();
+            }
+
+            dbCategory.Name = category.Name;
+            dbCategory.Url= category.Url;
+            dbCategory.Visible = category.Visible;
+
+            await _context.SaveChangesAsync();
+            return await GetAdminCategoriesAsync();
         }
     }
 }
